feb4f71d264890df5b4269cf1199fa529a2d4aa2
kardelenbicen <kardelen.bicen.tr@gmail.com>
2023-12-22 15:18:08 +0300

    First Commit

---
+++ Exercise 4-11/Exercise 4-11/Exercise 4-11.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hictory, dickory, dock,");
+	printf("The mouse ran up the clock.");
+	printf("The clock struck one,");
+	printf("The mouse ran down,");
+	printf("Hickory, dickory, dock.");
+	return(0);
+}
